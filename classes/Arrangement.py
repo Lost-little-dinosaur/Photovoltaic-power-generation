ceb66d0639f623846fbb42984d142b951fdb0e59
wjy <1471947151@qq.com>
2023-12-22 12:54:13 +0800

    add: arrangement

--- classes/Arrangement.py
+++ classes/Arrangement.py
@@ -3,13 +3,15 @@ from const.const import components
 
 class Arrangement:
     def __init__(self, specification, component, width, length, type):
-        self.specification = specification
-        self.component = component
-        self.type = type
-        self.width = width
-        self.length = length
-        self.array_x = []
-        self.array_y = []
+        self.specification = specification  # 排布的类型
+        self.component = component  # 使用组件的类型
+        self.type = type  # 排布的方式：基墩，膨胀常规，膨胀抬高
+        self.width = width  # 排布的宽度（上下）
+        self.length = length  # 排布的长度（左右）
+        self.array_x = []  # 竖梁相对位置
+        self.array_y = []  # 横梁相对位置
+        self.start_x = 0  # 排布左上角坐标x
+        self.start_y = 0  # 排布左上角坐标y
 
     def chooselayout(self):
         if self.specification == "竖二" and self.type == "基墩":
@@ -23,6 +25,9 @@ class Arrangement:
             layout = "Default Layout"
 
         return array_x, array_y
+    def draw(self, x, y):
+        self.start_x = x
+        self.start_y = y
 
 
 
